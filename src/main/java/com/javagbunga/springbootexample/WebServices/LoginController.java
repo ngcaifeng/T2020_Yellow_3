2f7a5579eb4d1d1eb16eac81f7dc714a2bcdef6e
GeniusPotatoes <GeniusPotatoes@users.noreply.github.com>
2019-10-31 11:23:38 +0800

    Customer model and Login Service

--- src/main/java/com/javagbunga/springbootexample/WebServices/LoginController.java
+++ src/main/java/com/javagbunga/springbootexample/WebServices/LoginController.java
@@ -4,7 +4,12 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.javagbunga.springbootexample.Common.CommonAPI;
+import com.javagbunga.springbootexample.Common.CommonResponseAPI;
 import com.javagbunga.springbootexample.WebModel.Customer;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
@@ -16,34 +21,33 @@ public class LoginController {
     private static final String PWD = "123";
 
     @RequestMapping("/login")
-    public String login(@RequestParam(value = "userName") String username,
+    public String login(@RequestParam(value = "userName") String userName,
                         @RequestParam(value = "password") String password) {
-        //This will be the API Call URL
-        final String uri = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/";
-        RestTemplate restTemplate = new RestTemplate();
 
-        //fetch user and convert to object
-        //Customer customer = restTemplate.getForObject(uri+username, String.class);
+        //Getting the request entity
+        HttpEntity<String> requestEntity = CommonAPI.getHtttpEntity();
+        //Modifying the URL
+        String requestURL = CommonAPI.getCustomerDetails + userName;
+        //Getting the responsible
+        ResponseEntity<String> result = CommonAPI.getHTTPGetResponse(requestURL,requestEntity);
 
-//        String result = restTemplate.getForObject(uri, String.class);
-//
-//        JsonArray jsonArray = new JsonParser().parse(result).getAsJsonArray();
-//        Iterator<JsonElement> iterator = jsonArray.iterator();
-//        while(iterator.hasNext()) {
-//            JsonElement element = iterator.next();
-//            JsonObject object = element.getAsJsonObject();
-//            if(object.get("id").getAsString().equals()){
-//                return object.get("name").getAsString();
-//            }
-//        }
-//        return "";
-//
-//        if(customer != null && password.equals(PWD)){]
-//            return "ok";
-//        }else{
-//            return "error";
-//        }
+        JsonObject jsonObject = new JsonParser().parse(result.getBody()).getAsJsonObject();
+        Iterator<JsonElement> iterator = jsonArray.iterator();
+        while(iterator.hasNext()) {
+            JsonElement element = iterator.next();
+            JsonObject object = element.getAsJsonObject();
+            if(object.get("id").getAsString().equals()){
+                return object.get("name").getAsString();
+            }
+        }
         return "";
+
+        if(customer != null && password.equals(PWD)){]
+            return "ok";
+        }else{
+            return "error";
+        }
+        return "OK";
     }
 }
 
